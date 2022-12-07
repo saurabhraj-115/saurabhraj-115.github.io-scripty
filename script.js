9831c4be81546302fa5a23f4023381a2335e7def
Saurabh Raj <saurabhraj115@gmail.com>
2022-12-08 03:25:19 +0530

    Scripty PRO

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
 function processScript(form){
+    var environ = form.envir.value;  
     var scriptInput = form.script.value;
     var arr = scriptInput.split("\"")
     var str1= "https://live-chat-static.sprinklr.com/test-html/index.html?appId="
-    var str2= "&env=prod12&skin=MODERN"
-    var finalUrl = str1.concat(arr[3],str2)
+    var str2= "&env="
+    var str3= "&skin=MODERN"
+    var finalUrl = str1.concat(arr[3],str2.concat(environ,str3))
     window.open(finalUrl);
 }
-
-  // function initFreshChat() {
-  //   window.fcWidget.init({
-  //     token: "124fc8a4-e7f2-42de-b17d-c62261d4e935",
-  //     host: "https://wchat.freshchat.com"
-  //   });
-  // }
-  // function initialize(i,t){var e;i.getElementById(t)?initFreshChat():((e=i.createElement("script")).id=t,e.async=!0,e.src="https://wchat.freshchat.com/js/widget.js",e.onload=initFreshChat,i.head.appendChild(e))}function initiateCall(){initialize(document,"freshchat-js-sdk")}window.addEventListener?window.addEventListener("load",initiateCall,!1):window.attachEvent("load",initiateCall,!1);
